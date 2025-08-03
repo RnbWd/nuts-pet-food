b1f38f65c0fc11a436982afd98e25106b5a66352
RnbWd <dwisner6@gmail.com>
2025-08-03 19:25:38 +0800

    add label to site

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     cssMinify: 'lightningcss',
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        label: resolve(__dirname, 'src/label.html'),
+      },
+    },
   },
   css: {
     transformer: 'lightningcss',
