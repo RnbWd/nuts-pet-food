385bb26a0c229ead5f1f6b6411e77869a47b892d
RnbWd <dwisner6@gmail.com>
2025-08-12 12:40:17 +0800

    updated vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'src/index.html'),
         label: resolve(__dirname, 'src/label.html'),
+        404: resolve(__dirname, 'src/404.html'),
+        v2: resolve(__dirname, 'src/v2.html'),
       },
     },
   },
