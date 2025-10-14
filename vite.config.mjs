6453eb5bdb1c2e0113b9d01825979da04ace3041
RnbWd <dwisner6@gmail.com>
2025-10-14 12:54:29 +0800

    remove unnecessary code

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,9 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        label: resolve(__dirname, 'src/label.html'),
         404: resolve(__dirname, 'src/404.html'),
-        v2: resolve(__dirname, 'src/v2.html'),
       },
     },
   },
